2c21f25b8a66048bdd6da3a20e057c4abe08e9c2
keerthivasan <47691119+keerthivasaa@users.noreply.github.com>
2021-05-17 02:29:52 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
 
+const houses = require('./routes/houses');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the house listing API');
+});
+
+app.use('/api/houses', houses);
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb+srv://keerthi-vasan_14:vasan14399@cluster0.k3os7.mongodb.net/myFirstDb?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(port, () => console.log(`Server is running on port ${port}`))
+    })
+    .catch(err => console.log(err))
